d36dacfca85dcda6e14a5454783353084200acd9
Lalit Mahajan <lalitmahajan444@gmail.com>
2026-09-25 13:04:26 +0530

    Remove old capital Frontend folder from repository

--- Frontend/src/app/(auth)/login.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
-import AuthScreen from '../../screens/AuthScreen';
-
-export default function LoginRoute() {
-  const { mode } = useLocalSearchParams<{ mode?: string }>();
-  const initialMode = mode === 'signup' ? 'signup' : 'signin';
-  return <AuthScreen initialMode={initialMode} />;
-}
--- Frontend/src/app/(auth)/signup.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AuthScreen from '../../screens/AuthScreen';
-
-export default function SignupRoute() {
-  return <AuthScreen initialMode="signup" />;
-}
